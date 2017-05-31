c7f8b8b7565edc3ffafac4d1da3aeb96478f576a
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-05-31 23:12:26 +0300

    [NIGHTLY]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import {WarningAlertComponent} from './warningalert/warningalert.component';
+import {SuccessAlertComponent} from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
