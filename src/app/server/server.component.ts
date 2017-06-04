75d0accf99b99955320bc039446571441141d921
Atanas Vasilev <a.vasilev@viscomp.bg>
2017-06-04 16:05:10 +0300

    [NIGHTLY]

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,6 @@ import {Component} from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
+  serverId:number = 3;
+  serverStatus:string = "offline";
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,18 +2,30 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
+  allowNewServer = false;
+
+  serverCreationStatus = 'No servers are created';
+
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
   }
 
 }
